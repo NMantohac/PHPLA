82c3e5f14335d6f84b2eb9e687f6b428718ebb9b
Norberto Mantohac <nmantohac82098@gmail.com>
2021-08-11 15:54:56 -0700

    proxy target update to process.env.PORT

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use('/email', createProxyMiddleware({
-    target: process.env.PORT ? 'process.env.PORT' : 'http://localhost:3001',
+    target: process.env.PORT,
     changeOrigin: true,
   }));
 };
