f3cda3d45e192575edafb2ce5fba420977856185
Norberto Mantohac <nmantohac82098@gmail.com>
2021-08-11 15:27:01 -0700

    proxy update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use('/email', createProxyMiddleware({
-    target: 'http://localhost:3001',
+    target: 'http://www.phplosangeles.org',
     changeOrigin: true,
   }));
 };
