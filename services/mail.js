ee35d8a7cd6e86c875f617b0ae8747083801134a
Norberto Mantohac <nmantohac82098@gmail.com>
2026-07-22 18:59:19 -0700

    removed auth word from mail.js, no need for app password to use OAuth2 google

--- services/mail.js
+++ services/mail.js
@@ -9,7 +9,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     type: 'OAUTH2',
     user: process.env.EMAIL,
-    pass: process.env.WORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
