6e36076c7ba9a1a8ee7124a17e6fb8237f65490b
Norberto Mantohac <nmantohac82098@gmail.com>
2026-08-06 19:16:12 -0700

    updated address

--- client/src/components/Footer/Footer.component.jsx
+++ client/src/components/Footer/Footer.component.jsx
@@ -28,8 +28,8 @@ const Footer = () => {
             <ul className="list-unstyled" style={{ fontSize: '15px' }}>
               <li>
                 <FontAwesomeIcon icon={faMapMarkerAlt} style={{ marginRight: '10px' }} />
-                <a href="https://www.google.com/maps/place/835+Wilshire+Blvd,+Los+Angeles,+CA+90017/@34.0500578,-118.2654015,2673m/data=!3m1!1e3!4m22!1m15!4m14!1m6!1m2!1s0x80c2c7b15e6b42ef:0xf32d2a0544270869!2s835+Wilshire+Boulevard,+Los+Angeles,+CA!2m2!1d-118.2588065!2d34.0499896!1m6!1m2!1s0x80c2c7b15e6b42ef:0xf32d2a0544270869!2s835+Wilshire+Blvd,+Los+Angeles,+CA+90017!2m2!1d-118.2588065!2d34.0499896!3m5!1s0x80c2c7b15e6b42ef:0xf32d2a0544270869!8m2!3d34.0499896!4d-118.2588065!16s%2Fg%2F11bw3_vx37?entry=ttu&g_ep=EgoyMDI1MDYwMi4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style={{ color: 'black' }}>
-                  835 Wilshire Blvd Ste 500, Los Angeles, CA 90017
+                <a href="https://www.google.com/maps/place/MCI+Center,+Los+Angeles,+CA+90017/@34.0479796,-118.2598885,18z/data=!3m1!4b1!4m10!1m2!2m1!1s700+S+Flower+St!3m6!1s0x80c2c7b6a739c46f:0x231985a1230c4ca2!8m2!3d34.0479796!4d-118.258601!15sCg83MDAgUyBGbG93ZXIgU3SSARFjb21wb3VuZF9idWlsZGluZ-ABAA!16s%2Fm%2F0416pvr?entry=ttu&g_ep=EgoyMDI2MDgwNC4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style={{ color: 'black' }}>
+                  700 S Flower St, Ste 1000, Los Angeles, CA 90017
                 </a>
               </li>
               <li>
